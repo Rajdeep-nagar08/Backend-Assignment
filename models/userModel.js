42684bd9065fc486e9228d9b8d486fa2d6499e17
Rajdeep Nagar <rajdeepnagar08@gmail.com>
2024-05-27 16:46:15 +0530

    User model created

---
+++ models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// user schema
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  photo: { type: String },
+  bio: { type: String },
+  phone: { type: String },
+  isPublic: { type: Boolean, default: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  googleId: { type: String },
+  facebookId: { type: String },
+  twitterId: { type: String },
+  githubId: { type: String }
+});
+
+module.exports = mongoose.model('User', userSchema);
