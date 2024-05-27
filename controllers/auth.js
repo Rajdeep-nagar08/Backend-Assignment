48a2e317fd9c2fdf2f34bb768892adc0e6149428
Rajdeep Nagar <rajdeepnagar08@gmail.com>
2024-05-27 17:41:23 +0530

    Setting Regix expression for Strong password Check

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,25 @@
 const User = require('../models/user');
-const { hashPassword, comparePassword } = require('../utils/passwordUtils');
-const { generateToken, verifyToken } = require('../utils/jwtUtils');
+const { hashPassword } = require('../utils/passwordUtils');
+const { generateToken } = require('../utils/jwtUtils');
 
 exports.register = async (req, res) => {
   try {
-    const { name, email, password, photo, bio, phone, isPublic, isAdmin} = req.body;
+    const { name, email, password, photo, bio, phone, isPublic, isAdmin } = req.body;
 
     // Check if email is valid
     if (!email.includes('@')) {
-      return res.status(400).json({ message: 'Please check your email address' });
+      return res.status(400).json({ message: 'Please enter a valid email address' });
+    }
+
+    // Check if user with the same email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User with this email already exists' });
+    }
+
+    // Check password complexity
+    if (!isStrongPassword(password)) {
+      return res.status(400).json({ message: 'Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character' });
     }
 
     // Set the user's role based on isAdmin
@@ -16,7 +27,6 @@ exports.register = async (req, res) => {
 
     const hashedPassword = await hashPassword(password);
     const user = new User({ name, email, password: hashedPassword, photo, bio, phone, isPublic, role });
-    
 
     const token = generateToken({ userId: user._id, role: user.role });
 
@@ -28,3 +38,7 @@ exports.register = async (req, res) => {
   }
 };
 
+function isStrongPassword(password) {
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{":;'?/>.<,])(?=.*\w).{8,}$/;
+  return passwordRegex.test(password);
+}
