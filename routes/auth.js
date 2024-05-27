bfecf8970b8001afc443621b19499b92bce451f7
Rajdeep Nagar <rajdeepnagar08@gmail.com>
2024-05-28 00:31:58 +0530

    API to user profiles

--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/register', auth.register);
 router.post('/login', auth.login);
 router.post('/logout', authenticateToken, auth.logout); 
+router.get('/profile', authenticateToken, auth.getProfile);
 
 module.exports = router;
